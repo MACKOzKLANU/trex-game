db030ae2b1e28761ce22b15f0933a64048685a01
Maciej S <115628283+MACKOzKLANU@users.noreply.github.com>
2023-11-04 21:54:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
-
 import turtle
 import random
 import math
 import time
-#okno
+
+# Create a window
 window = turtle.Screen()
 window.bgcolor("lightgreen")
 
-#create player
+# Create the player turtle
 player = turtle.Turtle()
 player.shape("triangle")
 player.color("blue")
-#disable the trail
 player.penup()
-#don't pause when changing directions
-player.speed(0)
-pspeed = 1
+player.speed(0)  # Set the initial speed to 0
+pspeed = 1  # Initial player speed
 player.speed(pspeed)
-enemspeed = 1
+enemspeed = 1  # Initial enemy speed
 
-#point
-# point = turtle.Turtle()
-# point.shape("circle")
-# point.color("yellow")
-# point.penup()
-# point.speed(0)
-# point.setposition(random.randint(-250, 250), random.randint(-250, 250))
+# Create enemy turtles
 p2 = turtle.Turtle()
 p2.shape("triangle")
 p2.color("red")
 p2.penup()
-
 p2.setposition(220, 0)
 p2.left(180)
-
-#disable the trail
-#don't pause when changing directions
-p2.speed(10)
+p2.speed(10)  # Set enemy speed
 
 p3 = turtle.Turtle()
 p3.shape("triangle")
 p3.color("red")
 p3.penup()
-
 p3.setposition(p2.xcor() + 50, 0)
 p3.left(180)
+p3.speed(5)  # Set enemy speed
 
-p3.speed(5)
-
-
-
-
-#draw border
+# Create a border
 pen = turtle.Turtle()
 pen.speed(0)
 pen.penup()
 pen.setposition(-300, -300)
 pen.pendown()
-#change pen size
 pen.pensize(5)
 pen.setposition(300, -300)
 pen.setposition(300, 300)
@@ -65,141 +47,82 @@ pen.setposition(-300, 300)
 pen.setposition(-300, -300)
 pen.setposition(-300, -5)
 pen.setposition(300, -5)
-
-
-
-
-#hide object
-
-
-
 pen.hideturtle()
-pen.penup()
+
+# Create a text display for points
 t = turtle.Turtle()
 t.setposition(150, 150)
 pointg = 0
 
-t.write(pointg, font=("Arial", 16, "bold"), align="center")
-t.clear()
-t.write(pointg, font=("Arial", 16, "bold"), align="center")
-
-t.hideturtle()
-
-
-#functions
+# Function for jumping
 jumped = False
 def jump():
     global jumped
-    if jumped == False:
+    if not jumped:
         jumped = True
         player.speed(0)
         player.dy = 30
-        
         player.sety(player.ycor() + player.dy)
         player.speed(pspeed)
-
         player.forward(70)
         player.sety(player.ycor() - player.dy)
         jumped = False
         player.speed(pspeed)
 
-
-
-
-
-
-
-    
-    
-
-#listen to our inputs
+# Listen for jump input
 turtle.listen()
 turtle.onkey(jump, "space")
-    
 
+# Main game loop
 while True:
-    
     p3.forward(1)
     p2.forward(enemspeed)
-
-
     player.forward(pspeed)
-    if player.xcor() >= 300 :
+
+    if player.xcor() >= 300:
         player.speed(0)
-        player.setposition(player.xcor()*(-1)+ 10, player.ycor())
-        if pspeed <=10:
+        player.setposition(player.xcor() * (-1) + 10, player.ycor())
+        if pspeed <= 10:
             pspeed += 0.1
             enemspeed += 0.1
-
         player.speed(pspeed)
         p2.speed(enemspeed)
-
         pointg += 1
         t.clear()
-
-
-        
-
         t.write(pointg, font=("Arial", 16, "bold"), align="center")
+
     if player.ycor() >= 300 or player.ycor() <= -300:
         player.speed(0)
-
-        player.setposition(player.xcor(), player.ycor()*(-1))
-        if pspeed <=10:
+        player.setposition(player.xcor(), player.ycor() * (-1))
+        if pspeed <= 10:
             pspeed += 0.1
             enemspeed += 0.1
-
         player.speed(pspeed)
         p2.speed(enemspeed)
-
         pointg += 1
         t.clear()
-
-        
-
         t.write(pointg, font=("Arial", 16, "bold"), align="center")
-        
-    #wad
+
     if p2.xcor() >= 300 or p2.xcor() <= -300:
-        p2.setposition(p2.xcor()*(-1), p2.ycor())
+        p2.setposition(p2.xcor() * (-1), p2.ycor())
     if p2.ycor() >= 300 or p2.ycor() <= -300:
-        p2.setposition(p2.xcor(), p2.ycor()*(-1))
-    
+        p2.setposition(p2.xcor(), p2.ycor() * (-1))
 
     if p3.xcor() >= 300 or p3.xcor() <= -300:
         p3.speed(0)
-        p3.setposition(p3.xcor()*(-1), p3.ycor())
+        p3.setposition(p3.xcor() * (-1), p3.ycor())
         p3.speed(1)
     if p3.ycor() >= 300 or p3.ycor() <= -300:
         p3.speed(0)
-
-        p2.setposition(p3.xcor(), p3.ycor()*(-1))
+        p2.setposition(p3.xcor(), p3.ycor() * (-1))
         p3.speed(1)
 
-
-
-    # if point.xcor() - player.xcor() <= 5 and point.ycor() - player.ycor() <= 5:
-    #     point.setposition(random.randint(-250, 250), random.randint(-250, 250))
-    # d = math.sqrt((player.xcor() - point.xcor())**2 + (player.ycor() - point.ycor())**2)
-    # if d <=15:
-    #     pointg +=1
-    #     point.setposition(random.randint(-250, 250), random.randint(-250, 250))
-    #     t.setposition(150, 150)
-    #     t.hideturtle()
-    #     
-    #przeszkadza
     d2 = math.sqrt((player.xcor() - p2.xcor())**2 + (player.ycor() - p2.ycor())**2)
     d3 = math.sqrt((player.xcor() - p3.xcor())**2 + (player.ycor() - p3.ycor())**2)
 
-    if d2 <=15 or d3 <=15:
+    if d2 <= 15 or d3 <= 15:
         t.hideturtle()
-        pointg -=1
+        pointg -= 1
         t.clear()
-
-        
-
         t.write(pointg, font=("Arial", 16, "bold"), align="center")
         t.hideturtle()
-
-
-
